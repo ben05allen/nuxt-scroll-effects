9dc6b3200e3a98a9625f90a3f0cbbc6c4ea95897
ben05allen <ben05allen@hotmail.com>
2023-08-06 11:42:40 +0900

    Adding curves

--- app.vue
+++ app.vue
@@ -2,16 +2,19 @@
   <div>
     <section class="mx-20 top">
       <h1 class="text-4xl text-red-400 my-40">This is a heading</h1>
-      <div class="my-40 bg-blue-400 py-72 w-full">a big blue box</div>
+      <div class="px-20 my-40 bg-blue-400 py-72 w-full">a big blue box</div>
     </section>
-    <section class="px-20 bubble">
-      <h2 class="text-2xl text-green-300">the next section</h2>
-      <ScrollEffect1>
-        <div class="my-40 bg-pink-300 py-24 text-center w-40">
-          a smaller red box
-        </div>
-      </ScrollEffect1>
-    </section>
+
+    <CurvedSection color="aliceblue">
+      <div class="px-40">
+        <h2 class="text-2xl text-green-300">the next section</h2>
+        <ScrollDelay>
+          <div class="my-40 bg-pink-300 py-24 text-center w-40">
+            a smaller red box
+          </div>
+        </ScrollDelay>
+      </div>
+    </CurvedSection>
 
     <section class="lastSection">This is the last section</section>
   </div>
@@ -29,32 +32,5 @@ section {
 }
 .lastSection::after {
   content: " 😀";
-}
-.bubble {
-  background-color: aliceblue;
-}
-.bubble::before {
-  content: "";
-  border-top-left-radius: 75% 100%;
-  border-top-right-radius: 75% 100%;
-  position: absolute;
-  top: -5em;
-  left: 0;
-  height: 5em;
-  width: 100%;
-  z-index: -1;
-  background-color: aliceblue;
-}
-.bubble::after {
-  content: "";
-  border-bottom-left-radius: 75% 100%;
-  border-bottom-right-radius: 75% 100%;
-  position: absolute;
-  bottom: -2em;
-  left: 0;
-  height: 2em;
-  width: 100%;
-  z-index: 1;
-  background-color: aliceblue;
 }
 </style>
---
+++ components/CurvedSection.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const props = defineProps({
+  color: String,
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="curved">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  --curved-color: v-bind(props.color);
+}
+.container {
+  position: relative;
+}
+.curved {
+  background-color: var(--curved-color);
+  width: 100vw;
+}
+.curved::before {
+  content: "";
+  border-top-left-radius: 75% 100%;
+  border-top-right-radius: 75% 100%;
+  position: absolute;
+  top: -5em;
+  left: 0;
+  height: 5em;
+  width: 100vw;
+  z-index: -1;
+  background-color: var(--curved-color);
+}
+.curved::after {
+  content: "";
+  border-bottom-left-radius: 75% 100%;
+  border-bottom-right-radius: 75% 100%;
+  position: absolute;
+  bottom: -2em;
+  left: 0;
+  height: 2em;
+  width: 100vw;
+  z-index: -1;
+  background-color: var(--curved-color);
+}
+</style>
